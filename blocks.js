66be9c21fa3203bfe4b83acd5402bd2acd824f06
Adrian Rossouw <adrian@developmentseed.org>
2011-03-23 10:26:07 +0200

    spacing

--- blocks.js
+++ blocks.js
@@ -1,17 +1,17 @@
 var el = require('expresslane'),
     document = require('document');
 
-    el.addBlock({
-        region: 'right',
-        loaders: [document.documentLoader],
-        filter: function(req, res) {
-            return !!req.doc;
-        },
-        content: function(req, res) {
-            return {
-                _template: 'docnav.jade',
-                doc: req.doc
-            };
-        }
+el.addBlock({
+    region: 'right',
+    loaders: [document.documentLoader],
+    filter: function(req, res) {
+        return !!req.doc;
+    },
+    content: function(req, res) {
+        return {
+            _template: 'docnav.jade',
+            doc: req.doc
+        };
+    }
 });
 
